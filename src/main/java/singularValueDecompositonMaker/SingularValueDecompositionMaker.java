52790ca92b4dea9221e82dc620c0447342eaedc3
= <priya.krish.6@gmail.com>
2014-11-18 22:30:23 -0800

    Added changes to SVD maker

--- src/main/java/singularValueDecompositonMaker/SingularValueDecompositionMaker.java
+++ src/main/java/singularValueDecompositonMaker/SingularValueDecompositionMaker.java
@@ -3,28 +3,15 @@ package singularValueDecompositonMaker;
 import Jama.Matrix;
 import Jama.SingularValueDecomposition;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.FileReader;
+import java.io.*;
 
 public class SingularValueDecompositionMaker {
     public static void main(String[] args) throws Exception {
-        String inputFileName = System.getProperty("inputFile");
-        String outputFileName = System.getProperty("outputFile");
 
-        if (inputFileName == null) {
-            throw new Exception("input file missing: " + inputFileName);
-        }
-
-        if (outputFileName == null) {
-            throw new Exception("output file missing: " + outputFileName);
-        }
-
-        String inputFile = System.getProperty("user.home") + "/data_files/" + inputFileName + ".csv";
+        String inputFile = System.getProperty("user.home") + "/data_files/" + "TagFeatureVectors_Horizontal_2" + ".csv";
 
 
-        String outputFile = System.getProperty("user.home") + "/data_files/" + outputFileName + ".csv";
+        String outputFile = System.getProperty("user.home") + "/data_files/" + "FeatureFileAfterSVD" + ".csv";
 
         FileReader reader = new FileReader(inputFile);
         BufferedReader bufferedReader = new BufferedReader(reader);
@@ -33,23 +20,45 @@ public class SingularValueDecompositionMaker {
         String header = bufferedReader.readLine();
         String[] fields = header.split(",");
 
-        Matrix matrix = new Matrix((int) bufferedReader.lines().count(),fields.length);
+        int numberOfFields = fields.length;
+        Matrix matrix = new Matrix((int) bufferedReader.lines().count(), numberOfFields);
 
         String line = null ;
 
-        while((line=bufferedReader.readLine())!=null){
+        //Skip the first line
+        bufferedReader.readLine();
+
+        while((line=bufferedReader.readLine())!=null) {
             String[] cols = line.split(",");
-            for(int j=0; j<cols.length;j++){
-                matrix.set(i,j,Double.parseDouble(cols[j]));
+            for (int j = 0; j < cols.length; j++) {
+                matrix.set(i, j, Double.parseDouble(cols[j]));
             }
             i++;
         }
         bufferedReader.close();
-        FileOutputStream fileOutputStream = new FileOutputStream(new File(outputFile));
 
-        SingularValueDecomposition singularValueDecomposition = new SingularValueDecomposition(matrix);
 
-        fileOutputStream.write(singularValueDecomposition.getU().toString().getBytes());
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(outputFile));
+        SingularValueDecomposition singularValueDecomposition = matrix.svd();
+
+        //Fill in the first row of the file with column headers
+        for(int k=0;k<100;k++){
+            bufferedWriter.write("Component"+k+",");
+        }
+
+        bufferedWriter.write("\n");
+
+        Matrix U = singularValueDecomposition.getU();
+
+        for (int j = 0; j < U.getRowDimension(); j++) {
+            for(int k=0;k<100;k++){
+                bufferedWriter.write(Double.toString(U.get(j,k)));
+                bufferedWriter.write(",");
+            }
+
+            bufferedWriter.write("\n");
+        }
+
+        bufferedWriter.close();
     }
-
 }
