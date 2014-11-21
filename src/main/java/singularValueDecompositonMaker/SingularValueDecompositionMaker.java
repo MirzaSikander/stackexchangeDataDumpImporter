a213396e6ad3872f39c44ecff0acaebea4b6700a
= <priya.krish.6@gmail.com>
2014-11-20 23:46:08 -0800

    Added code to print out u,v and singular matrices

--- src/main/java/singularValueDecompositonMaker/SingularValueDecompositionMaker.java
+++ src/main/java/singularValueDecompositonMaker/SingularValueDecompositionMaker.java
@@ -8,10 +8,12 @@ import java.io.*;
 public class SingularValueDecompositionMaker {
     public static void main(String[] args) throws Exception {
 
-        String inputFile = System.getProperty("user.home") + "/data_files/" + "TagFeatureVectors_Horizontal_2" + ".csv";
+        String inputFile = System.getProperty("user.home") + "/data_files/" + System.getProperty("inputFile") + ".csv";
 
+        String outputFileForU = System.getProperty("user.home") + "/data_files/" + System.getProperty("outputFileForU") + ".csv";
+        String outputFileForV = System.getProperty("user.home") + "/data_files/" + System.getProperty("outputFileForV") + ".csv";
+        String outputFileForS = System.getProperty("user.home") + "/data_files/" + System.getProperty("outputFileForS") + ".csv";
 
-        String outputFile = System.getProperty("user.home") + "/data_files/" + "FeatureFileAfterSVD" + ".csv";
 
         FileReader reader = new FileReader(inputFile);
         BufferedReader bufferedReader = new BufferedReader(reader);
@@ -22,43 +24,66 @@ public class SingularValueDecompositionMaker {
 
         int numberOfFields = fields.length;
         Matrix matrix = new Matrix((int) bufferedReader.lines().count(), numberOfFields);
+        bufferedReader.close();
 
         String line = null ;
+        BufferedReader bufferedReader1 = new BufferedReader(new FileReader(inputFile));
+
 
         //Skip the first line
-        bufferedReader.readLine();
+        bufferedReader1.readLine();
 
-        while((line=bufferedReader.readLine())!=null) {
+        while((line=bufferedReader1.readLine())!=null) {
             String[] cols = line.split(",");
             for (int j = 0; j < cols.length; j++) {
                 matrix.set(i, j, Double.parseDouble(cols[j]));
             }
             i++;
         }
-        bufferedReader.close();
+        bufferedReader1.close();
 
 
-        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(outputFile));
+        BufferedWriter bufferedWriterForU = new BufferedWriter(new FileWriter(outputFileForU));
+        BufferedWriter bufferedWriterForV = new BufferedWriter(new FileWriter(outputFileForV));
+        BufferedWriter bufferedWriterForS = new BufferedWriter(new FileWriter(outputFileForS));
+
         SingularValueDecomposition singularValueDecomposition = matrix.svd();
 
-        //Fill in the first row of the file with column headers
-        for(int k=0;k<100;k++){
-            bufferedWriter.write("Component"+k+",");
-        }
-
-        bufferedWriter.write("\n");
-
         Matrix U = singularValueDecomposition.getU();
+        Matrix V = singularValueDecomposition.getV();
+        double[] S = singularValueDecomposition.getSingularValues();
 
-        for (int j = 0; j < U.getRowDimension(); j++) {
-            for(int k=0;k<100;k++){
-                bufferedWriter.write(Double.toString(U.get(j,k)));
-                bufferedWriter.write(",");
-            }
 
-            bufferedWriter.write("\n");
+        //Fill in the first row of the file with column headers
+
+        for(int k=0;k<U.getArray()[1].length;k++){
+            bufferedWriterForU.write("Component" + k + ",");
         }
 
-        bufferedWriter.close();
+        bufferedWriterForU.write("\n");
+
+        for (int j = 0; j < U.getArray().length; j++) {
+            for(int k=0;k<U.getArray()[j].length;k++){
+                bufferedWriterForU.write(Double.toString(U.get(j, k)));
+                bufferedWriterForU.write(",");
+            }
+            bufferedWriterForU.write("\n");
+        }
+        bufferedWriterForU.close();
+
+        for (int j = 0; j < V.getArray().length; j++) {
+            for(int k=0;k<V.getArray()[j].length;k++){
+                bufferedWriterForV.write(Double.toString(V.get(j, k)));
+                bufferedWriterForV.write(",");
+            }
+            bufferedWriterForV.write("\n");
+        }
+        bufferedWriterForV.close();
+
+        for (int j = 0; j < S.length; j++) {
+                bufferedWriterForS.write(Double.toString(S[j]));
+                bufferedWriterForS.write(",");
+            }
+        bufferedWriterForS.close();
     }
 }
