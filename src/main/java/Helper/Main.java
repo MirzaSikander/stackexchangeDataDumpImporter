e013237bf55a91bc0016b08835bda0acec48861b
Mirza@air <mirzasikander@outlook.com>
2014-12-01 23:23:55 -0800

    modified results compiler

--- src/main/java/Helper/Main.java
+++ src/main/java/Helper/Main.java
@@ -12,6 +12,8 @@ public class Main {
 
         System.out.println();
 
+
+
         for(File file: files){
             SortFile sortFile = new SortFile(directoryPath, file.getName(), noOfRelevantPosts);
             sortFile.extractFile();
--- src/main/java/Helper/SortFile.java
+++ src/main/java/Helper/SortFile.java
@@ -2,6 +2,7 @@ package Helper;
 
 import java.io.*;
 import java.util.Comparator;
+import java.util.PriorityQueue;
 import java.util.TreeMap;
 
 public class SortFile {
@@ -13,21 +14,30 @@ public class SortFile {
     public static final int USER_ID_EXPERTS_INDEX = 11;
     public int noOfRelevantPosts = 10;
 
-    TreeMap<String, String> treeMap = new TreeMap<>(new Comparator() {
+    class Tuple{
+        Double rank;
+        String question;
+
+        public Tuple(String rank, String question){
+            this.rank = Double.parseDouble(rank);
+            this.question = question;
+        }
+    }
+
+    PriorityQueue<Tuple> queue = new PriorityQueue<>(new Comparator() {
         @Override
         public int compare(Object o1, Object o2) {
-            if (o1 instanceof String && o2 instanceof String) {
-                double o1_score = Double.parseDouble((String) o1);
-                double o2_score = Double.parseDouble((String) o2);
+            if (o1 instanceof Tuple && o2 instanceof Tuple) {
+                double o1_score = ((Tuple) o1).rank;
+                double o2_score = ((Tuple) o2).rank;
 
-                return Double.compare(o1_score, o2_score);
+                return -1 * Double.compare(o1_score, o2_score);
             }
             else{
                 throw new IllegalArgumentException("parameter not string");
             }
         }
     });
-
 
     public SortFile(String directoryPath, String inputFileName, String noOfRelevantPosts) throws FileNotFoundException {
         this.inputFileName = inputFileName;
@@ -41,7 +51,7 @@ public class SortFile {
         bufferedReader = new BufferedReader(new FileReader(this.inputFile));
         String line;
         while ((line = bufferedReader.readLine()) != null) {
-            treeMap.put(line.split(",")[0], line);
+            queue.add(new Tuple(line.split(",")[0], line));
         }
         bufferedReader.close();
     }
@@ -50,7 +60,7 @@ public class SortFile {
         int count = 0;
 
         for(int i=0; i< noOfRelevantPosts; i++){
-            String testQuestion = treeMap.pollFirstEntry().getValue();
+            String testQuestion = queue.poll().question;
             String userField = testQuestion.split(",")[USER_ID_EXPERTS_INDEX];
             String[] users = userField.substring(1, userField.length()-1).split(";");
             for(String user:users){
